99642a58d4b0899a5fab5d7edad3e2c1c37b7537
Scott Haleen <scott.haleen@gmail.com>
2019-01-11 21:37:51 -0500

    fix travis

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/scotthaleen/jgroups-logger"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/tools.logging "0.4.1"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/tools.logging "0.4.1"]
                  [org.jgroups/jgroups "4.0.15.Final"]]
   :plugins [[lein-shell "0.5.0"]
             [lein-ancient "0.6.15"]
@@ -11,10 +12,7 @@
   :aot :all
   :profiles {:dev {:dependencies [[log4j/log4j "1.2.17"]
                                   [org.slf4j/slf4j-log4j12 "1.7.25"]]}
-             :provided [:project/provided]
-             :lint [:project/provided :project/lint]
-             :project/provided {:dependencies [[org.clojure/clojure "1.10.0"]]}
-             :project/lint {:plugins [[jonase/eastwood "0.3.4"]]}}
+             :lint {:plugins [[jonase/eastwood "0.3.4"]]}}
   :aliases {"lint" ["with-profile" "lint" "eastwood"]
             "travis-ci" ["do" ["clean"] ["test"] ["jar"]]})
 
